a55c6734a1e7efba7600eee7b883c21ab28eb9df
Majid <majid.mohammadi11@gmail.com>
2023-08-01 02:31:55 -0400

    add count function in Main Model file

--- Models/Home.php
+++ Models/Home.php
@@ -1,9 +1,16 @@
 <?php
 class HomeModel extends MainModel
 {
+    private $table = 'users';
+
+    public function __construct()
+    {
+        parent::__construct($this->table);
+    }
+
     public function selectById($id)
     {
-        return $this->db->get('users', '*', ['id'=>$id]);
+        return $this->db->get($this->table, '*', ['id'=>$id]);
     }
 
 }
--- Models/Main.php
+++ Models/Main.php
@@ -3,15 +3,22 @@ class MainModel extends App
 {
     protected $db;
     protected $logs;
+    private $tableName;
     
-    public function __construct()
+    public function __construct($tableName)
     {
         $this->db = $this->load('syslibs', 'db');
+        $this->tableName = $tableName;
     }
 
     public function get($object, $where)
     {
         return $this->db->get($object, '*', [$where['key']=> $where['value']]);
+    }
+
+    public function count()
+    {
+        return $this->db->count($this->tableName);
     }
 
 }
